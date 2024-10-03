3497dfffb7b0669007edc2da0464c4d9312ce316
belramello <belenramello@gmail.com>
2024-10-02 21:47:17 -0300

    subiendo inicio de iniciarSesion

--- src/app/(tabs)/iniciarSesion.tsx
+++ src/app/(tabs)/iniciarSesion.tsx
@@ -1,24 +1,103 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextInput, Button} from 'react-native';
 import { Text, View } from '@/src/components/Themed';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'expo-router';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { createIconSetFromFontello } from '@expo/vector-icons';
 
 //ESTA ES LA PANTALLA DEL CALENDARIO
-export default function IniciarSesionScreen() {
+
+
+const IniciarSesion = () =>{
+  const [user, SetUser]= useState('');
+  const [password, SetPassword]= useState('');
+  const [errors, setErrors] = useState('');
+  const router=useRouter()
+  
+  
+  const validarInput = () => {
+    setErrors('');
+    if (!user) {
+      setErrors('te falta el user boludito');
+      return false;
+    }
+    if (!password) {
+      setErrors('te falta la contraseña retardado');
+      return false;
+    }
+
+    return true;
+  };
+
+  const onLogin = () => {
+    if (!validarInput()) {
+      return;
+    }
+    router.push('/documentos');
+
+    
+  };
+
+  
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Bienvenida señora zoy</Text>
+      
+
+      <Text style={styles.label}>email</Text>
+      <TextInput
+        value={user}
+        onChangeText={SetUser}
+        placeholder="ejemplo@gmail.com"
+        style={styles.input}
+      />
+
+      <Text style={styles.label}>password ($)</Text>
+      <TextInput
+        value={password}
+        onChangeText={SetPassword}
+        placeholder="*****************"
+        style={styles.input}
+      />
+      <Text style={styles.error}>{errors}</Text>
+      <Button onPress={onLogin} title="Iniciar sesión" />
+
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
   },
-  title: {
-    fontSize: 30,
+  image: {
+    width: '50%',
+    aspectRatio: 1,
+    alignSelf: 'center',
+  },
+  textButton: {
+    alignSelf: 'center',
     fontWeight: 'bold',
-    color: '#778c43'
+    color: Colors.light.tint,
+    marginVertical: 10,
+  },
+  label: {
+    color: 'gray',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginTop: 5,
+    marginBottom: 20,
+    backgroundColor: 'white',
+    borderRadius: 5,
+  },
+  error: {
+    color: 'red',
+    textAlign: 'center',
   },
 });
+
+export default IniciarSesion;
