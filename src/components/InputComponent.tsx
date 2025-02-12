2da48841edac46ad7ca7f4d7bc0ee7a89b877089
carocor <126020764+carocor1@users.noreply.github.com>
2025-02-12 17:43:12 -0300

    Actualizando componentes de inputs que poseían errores

--- src/components/InputComponent.tsx
+++ src/components/InputComponent.tsx
@@ -1,50 +1,59 @@
-import { View, Text, TextInput, StyleSheet, TextStyle, ViewStyle } from "react-native";
-
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TextStyle,
+  ViewStyle,
+} from "react-native";
 
 type InputComponentProps = {
-    label: string;
-    value: string;
-    setFunction: (text: string) => void;
-    style?: ViewStyle | TextStyle;
+  label: string;
+  value: string;
+  setFunction: (text: string) => void;
+  style?: ViewStyle | TextStyle;
 };
 
-const InputComponent: React.FC<InputComponentProps> = ({ label, value, setFunction, style }) => {
-    return (
-        <View style={styles.inputGroup}>
-            <Text style={styles.label}>{label}</Text>
-            <TextInput
-                value={value}                    // Se asigna el valor actual del input
-                onChangeText={setFunction}       // Se pasa la función para actualizar el valor
-                placeholder={label}              // Usa el label como placeholder
-                style={[styles.input, style]}
-            />
-        </View>
-    );
+const InputComponent: React.FC<InputComponentProps> = ({
+  label,
+  value,
+  setFunction,
+  style,
+}) => {
+  return (
+    <View style={styles.inputGroup}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput
+        value={value} // Se asigna el valor actual del input
+        onChangeText={setFunction} // Se pasa la función para actualizar el valor
+        placeholder={label} // Usa el label como placeholder
+        style={[styles.input]}
+      />
+    </View>
+  );
 };
-
 
 const styles = StyleSheet.create({
-    inputGroup: {
-        alignItems: 'center',
-        marginBottom: 20,
-      },
-      label: {
-        color: '#000',
-        fontSize: 14,
-        fontWeight: 'bold',
-        alignSelf: 'flex-start',
-        marginBottom: 5,
-        marginLeft: '10%',
-      },
-      input: {
-        width: '80%',
-        borderWidth: 1,
-        borderColor: 'gray',
-        padding: 10,
-        backgroundColor: 'white',
-        borderRadius: 20,
-      },
-
-})
+  inputGroup: {
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  label: {
+    color: "#000",
+    fontSize: 14,
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+    marginBottom: 5,
+    marginLeft: "10%",
+  },
+  input: {
+    width: "80%",
+    borderWidth: 1,
+    borderColor: "gray",
+    padding: 10,
+    backgroundColor: "white",
+    borderRadius: 20,
+  },
+});
 
 export default InputComponent;
--- src/components/SaveButton.tsx
+++ src/components/SaveButton.tsx
@@ -1,33 +1,33 @@
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 type SaveButtonProps = {
-    texto: string;
-    onPress: () => void; 
+  texto: string;
+  onPress: () => void;
 };
 
 const SaveButton: React.FC<SaveButtonProps> = ({ texto, onPress }) => {
-    return (
-        <TouchableOpacity style={styles.saveButton} onPress={onPress}> 
-          <Text style={styles.buttonText}>{texto}</Text>
-        </TouchableOpacity>
-    );
+  return (
+    <TouchableOpacity style={styles.saveButton} onPress={onPress}>
+      <Text style={styles.buttonText}>{texto}</Text>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    saveButton: {
-        backgroundColor: '#DF732E', //color de fondo naranja
-        paddingVertical: 15, // Aumenta el padding vertical
-        paddingHorizontal: 30, // Aumenta el padding horizontal
-        borderRadius: 25, // Aumenta el borderRadius
-        marginLeft: 10,
-        width: '45%', // Aumenta el ancho del botón
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: 'bold',
-        textAlign: 'center',
-        fontSize:17, // Centra el texto en el botón
-    },
+  saveButton: {
+    backgroundColor: "#DF732E",
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 25,
+    marginLeft: 10,
+    width: "45%",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 17,
+  },
 });
 
 export default SaveButton;
