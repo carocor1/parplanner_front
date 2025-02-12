110f6da911619da426f511658513a4c4b505ddc3
carocor <126020764+carocor1@users.noreply.github.com>
2025-02-12 03:11:39 -0300

    Definiendo nuevas pantallas (registroHijoOIngresoCódigo y vinculacionHijoORegistroHijo) a seguir desarrollando.

---
+++ src/app/registroHijoOIngresoCodigo.tsx
@@ -0,0 +1,12 @@
+import { Text } from "../components/Themed";
+
+const registroHijoOIngresoCodigoScreen = () => {
+  return (
+    <Text>
+      Registra a tu hijo, y luego envíale un código de invitación al otro
+      progenitor para que se asocie
+    </Text>
+  );
+};
+
+export default registroHijoOIngresoCodigoScreen;
---
+++ src/app/vinculacionHijoORegistroHijo.tsx
@@ -0,0 +1,7 @@
+import { Input } from "react-native-elements";
+
+const vinculacionHijoORegistroHijoScreen = () => {
+  return <Input>Registrate</Input>;
+};
+
+export default vinculacionHijoORegistroHijoScreen;
