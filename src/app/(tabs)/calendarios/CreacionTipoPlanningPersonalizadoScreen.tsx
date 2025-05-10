3fbe40bf83c1e10a13d05cc7b4e6e800f91f92cc
carocor <126020764+carocor1@users.noreply.github.com>
2025-05-10 12:43:59 -0300

    Arreglando pantalla de creación de tipo de planning personalizado.

--- src/app/(tabs)/calendarios/CreacionTipoPlanningPersonalizadoScreen.tsx
+++ src/app/(tabs)/calendarios/CreacionTipoPlanningPersonalizadoScreen.tsx
@@ -1,31 +1,34 @@
-import { StyleSheet, View, TextInput, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState } from "react";
 import { useLocalSearchParams, useRouter } from "expo-router";
-import Calendario from "@/src/components/ComponenteCalendario";
 import Colors from "@/src/constants/Colors";
 import { ALERT_TYPE, Toast } from "react-native-alert-notification";
 import { registrarTipoPlanning } from "@/src/services/tipoPlanningService";
 import CustomButton from "@/src/components/CustomButton";
+
 const CreacionTipoPlanningPersonalizadoScreen = () => {
-  const [nombre, setNombre] = useState("");
-  const [fechasCreador, setFechasCreador] = useState<string[]>([]);
-  const [fechasParticipante, setFechasParticipante] = useState<string[]>([]);
   const searchParams = useLocalSearchParams();
-  const fechaInicio = useLocalSearchParams() as {
-    fechaInicio?: string;
-  };
   const router = useRouter();
+  const nombreCreador = "VOS";
+  const nombreParticipante = "O";
+  const [nombre, setNombre] = useState("");
+  const [dias, setDias] = useState<(string | null)[]>(
+    Array(14).fill("creador")
+  );
 
   const validateInput = () => {
     const validationRules = [
       { condition: !nombre, message: "El título es requerido" },
       {
-        condition: fechasCreador.length === 0,
-        message: "El creador no ha seleccionado ninguna fecha.",
-      },
-      {
-        condition: fechasParticipante.length === 0,
-        message: "El participante no ha seleccionado ninguna fecha.",
+        condition: dias.some((dia) => dia === null),
+        message: "Todos los días deben estar asignados a un progenitor.",
       },
     ];
 
@@ -42,154 +45,145 @@ const CreacionTipoPlanningPersonalizadoScreen = () => {
     return true;
   };
 
-  // Función para calcular el vector intercalado
-  const calcularVectorIntercalado = (
-    fechasCreador: string[],
-    fechasParticipante: string[]
-  ): number[] => {
-    let resultado: number[] = [];
-
-    const calcularDiasConsecutivos = (fechas: string[]): number[] => {
-      if (fechas.length === 0) return [];
-
-      const fechasOrdenadas = fechas.sort(
-        (a, b) => new Date(a).getTime() - new Date(b).getTime()
-      );
-      let grupos: number[] = [];
-      let contador = 1;
-      for (let i = 1; i < fechasOrdenadas.length; i++) {
-        const fechaActual = new Date(fechasOrdenadas[i]);
-        const fechaAnterior = new Date(fechasOrdenadas[i - 1]);
-        const diferenciaDias =
-          (fechaActual.getTime() - fechaAnterior.getTime()) /
-          (1000 * 60 * 60 * 24);
-
-        if (diferenciaDias === 1) {
-          contador++;
-        } else {
-          grupos.push(contador);
-          contador = 1;
-        }
-      }
-      grupos.push(contador);
-      return grupos;
-    };
-
-    const gruposCreador = calcularDiasConsecutivos(fechasCreador);
-    const gruposParticipante = calcularDiasConsecutivos(fechasParticipante);
-
-    const maxLength = Math.max(gruposCreador.length, gruposParticipante.length);
-    for (let i = 0; i < maxLength; i++) {
-      if (i < gruposCreador.length) resultado.push(gruposCreador[i]);
-      if (i < gruposParticipante.length) resultado.push(gruposParticipante[i]);
-    }
-    return resultado;
+  const toggleDia = (index: number, progenitor: string) => {
+    setDias((prev) => {
+      const newDias = [...prev];
+      newDias[index] = progenitor;
+      return newDias;
+    });
   };
 
-  const evaluarVector = (vector: number[]): number[] => {
-    const contador = vector.length;
-    if (contador < 2) {
-      return [];
-    }
-
-    let suma = vector.reduce((acc, num) => acc + num, 0);
-
-    if (suma < 14) {
-      let nuevovector = [...vector];
-      let contador2 = 0;
-
-      while (suma < 14 && suma + vector[contador2] <= 14) {
-        nuevovector.push(vector[contador2]);
-        suma += vector[contador2];
-        contador2 = (contador2 + 1) % vector.length;
-      }
-
-      if (suma === 14) {
-        return nuevovector;
-      } else {
-        return [];
-      }
-    } else if (suma === 14) {
-      return vector;
-    } else {
-      return [];
-    }
+  const calcularPorcentaje = (progenitor: string): number => {
+    const totalDias = dias.length;
+    const diasAsignados = dias.filter((dia) => dia === progenitor).length;
+    return Math.round((diasAsignados / totalDias) * 100);
   };
 
   const crearTipoPlanning = async () => {
     if (!validateInput()) {
       return;
     }
-    const vectorIntercalado = calcularVectorIntercalado(
-      fechasCreador,
-      fechasParticipante
-    );
-    const evaluacion = evaluarVector(vectorIntercalado);
-    if (evaluacion.length === 0) {
-      Toast.show({
-        type: ALERT_TYPE.WARNING,
-        title: "Error",
-        textBody: "La distribución no es equitativa",
-      });
-      return;
-    } else {
-      try {
-        const response = await registrarTipoPlanning(nombre, evaluacion);
-        if (response.id) {
-          Toast.show({
-            type: ALERT_TYPE.SUCCESS,
-            title: "Éxito",
-            textBody: "Tu tipo de planning se guardó correctamente.",
-          });
-          router.push({
-            pathname: "/(tabs)/calendarios/CreacionPlanningScreen",
-            params: {
-              planningId: response.id,
-              planningRechazandoId: searchParams.planningRechazado,
-              fechaInicio: fechaInicio?.fechaInicio,
-              nombreTipoPlan: response.nombre,
-              planningExpiradoId: searchParams.planningExpiradoId,
-            },
-          });
-        }
-        setNombre("");
-        setFechasCreador([]);
-        setFechasParticipante([]);
-      } catch (error) {
+
+    const vector: number[] = [];
+    let contador = 1;
+
+    for (let i = 1; i < dias.length; i++) {
+      if (dias[i] === dias[i - 1]) {
+        contador++;
+      } else {
+        vector.push(contador);
+        contador = 1;
+      }
+    }
+    vector.push(contador);
+    console.log(vector);
+
+    try {
+      const response = await registrarTipoPlanning(nombre, vector);
+      if (response.id) {
         Toast.show({
-          type: ALERT_TYPE.DANGER,
-          title: "Error",
-          textBody:
-            "Hubo un problema al guardar la planificación. Por favor, inténtalo de nuevo.",
+          type: ALERT_TYPE.SUCCESS,
+          title: "Éxito",
+          textBody: "Tu tipo de planning se guardó correctamente.",
+        });
+        router.push({
+          pathname: "/(tabs)/calendarios/CreacionPlanningScreen",
+          params: {
+            planningId: response.id,
+            planningRechazandoId: searchParams.planningRechazado,
+            fechaInicio: searchParams.fechaInicio,
+            nombreTipoPlan: response.nombre,
+            planningExpiradoId: searchParams.planningExpiradoId,
+          },
         });
       }
+      setNombre("");
+      setDias(Array(14).fill("creador"));
+    } catch (error) {
+      Toast.show({
+        type: ALERT_TYPE.DANGER,
+        title: "Error",
+        textBody:
+          "Hubo un problema al guardar la planificación. Por favor, inténtalo de nuevo.",
+      });
     }
   };
 
   return (
-    <ScrollView style={styles.container}>
-      <View>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
         <View style={styles.inputContainer}>
           <TextInput
             style={[styles.input]}
-            placeholder="Nombre"
+            placeholder="Título del tipo de planning"
             value={nombre}
             onChangeText={setNombre}
           />
         </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.titleText}>
+            INGRESÁ LA DISTRIBUCIÓN DE LOS DÍAS DEL TIPO DE PLANNING
+          </Text>
+          <Text style={styles.centeredText}>
+            Para ello, seleccioná los días que te gustaría que tu hijo pase con
+            el otro progenitor.
+          </Text>
+          <Text style={styles.italicText}>
+            Por ejemplo, si quieres que tu hijo pase 7 días consecutivos contigo
+            y luego 7 días con el otro progenitor, seleccioná los últimos 7
+            círculos.
+          </Text>
+        </View>
+        <View style={styles.circulosContainer}>
+          {dias.map((dia, index) => (
+            <TouchableOpacity
+              key={index}
+              style={[
+                styles.circulo,
+                dia === "creador" && styles.circuloCreador,
+                dia === "participante" && styles.circuloParticipante,
+              ]}
+              onPress={() => {
+                const nuevoProgenitor =
+                  dia === "creador" ? "participante" : "creador";
+                toggleDia(index, nuevoProgenitor);
+              }}
+            >
+              <Text style={styles.circuloTexto}>
+                {dia === "creador"
+                  ? nombreCreador.charAt(0).toUpperCase()
+                  : nombreParticipante.charAt(0).toUpperCase()}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
 
-        <View style={styles.calendarioContainer}>
-          <Calendario
-            onSelectDatesCreator={(fechas) => setFechasCreador(fechas)}
-            onSelectDatesParticipant={(fechas) => setFechasParticipante(fechas)}
-          />
+        <View style={styles.legendContainer}>
+          <View style={styles.legendItem}>
+            <View style={[styles.legendCircle, styles.circuloCreador]} />
+            <View>
+              <Text style={styles.legendText}>Vos</Text>
+              <Text style={styles.percentageText}>
+                {calcularPorcentaje("creador")}%
+              </Text>
+            </View>
+          </View>
+          <View style={styles.legendItem}>
+            <View style={[styles.legendCircle, styles.circuloParticipante]} />
+            <View>
+              <Text style={styles.legendText}>Otro Progenitor</Text>
+              <Text style={styles.percentageText}>
+                {calcularPorcentaje("participante")}%
+              </Text>
+            </View>
+          </View>
         </View>
 
         <CustomButton
           onPress={crearTipoPlanning}
           title="REGISTRAR TIPO DE PLANNING"
-          backgroundColor={Colors.marron.marronClaro}
-          textColor={Colors.marron.marronNormal}
+          backgroundColor={Colors.azul.azulOscuro}
+          textColor={"white"}
         ></CustomButton>
       </View>
     </ScrollView>
@@ -199,24 +193,98 @@ const CreacionTipoPlanningPersonalizadoScreen = () => {
 export default CreacionTipoPlanningPersonalizadoScreen;
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: "space-between",
+  },
   container: {
     flex: 1,
     padding: 20,
     backgroundColor: "white",
   },
   inputContainer: {
+    marginTop: 20,
     marginBottom: 15,
     width: "100%",
-  },
-  calendarioContainer: {
-    flex: 1,
-    width: "100%",
-    marginBottom: 20,
   },
   input: {
     padding: 10,
     borderWidth: 1,
     borderColor: "#ccc",
     borderRadius: 10,
+  },
+  circulosContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  circulo: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "#ccc",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  circuloCreador: {
+    backgroundColor: Colors.marron.marronClaro,
+  },
+  circuloParticipante: {
+    backgroundColor: Colors.lila.lilaClaro,
+  },
+  circuloTexto: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  textContainer: {
+    marginTop: 50,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  centeredText: {
+    fontSize: 16,
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  italicText: {
+    fontSize: 14,
+    fontStyle: "italic",
+    textAlign: "center",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  legendContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginBottom: 60,
+  },
+  legendItem: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  legendCircle: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    marginRight: 5,
+  },
+  legendText: {
+    fontSize: 14,
+    color: Colors.gris.oscuro,
+  },
+  percentageText: {
+    fontSize: 12,
+    color: Colors.gris.oscuro,
+    textAlign: "center",
+    marginTop: 2,
   },
 });
--- src/services/tipoPlanningService.ts
+++ src/services/tipoPlanningService.ts
@@ -2,30 +2,27 @@ import { TipoPlanning } from "../interfaces/TipoPlanning";
 import api from "./api";
 
 export const getTipoPlanning = async (): Promise<TipoPlanning[]> => {
-    try {
-      const response = await api.get("/tipo-planning");
-      const plannings: TipoPlanning[] = response.data;
-      return plannings;
-    } catch (error) {
-      console.error("Error al obtener los plannings:", error);
-      throw error;
-    }
+  try {
+    const response = await api.get("/tipo-planning");
+    const plannings: TipoPlanning[] = response.data;
+    return plannings;
+  } catch (error) {
+    console.error("Error al obtener los plannings:", error);
+    throw error;
+  }
 };
-  
 
 export const registrarTipoPlanning = async (
-nombre: string, evaluacion: number[]
-  
+  nombre: string,
+  distribucion: number[]
 ) => {
   try {
-    
     const response = await api.post("/tipo-planning", {
-    nombre,
-    distribucion:evaluacion,
+      nombre,
+      distribucion,
     });
     return response.data;
   } catch (error) {
     console.error("Error al registrar el tipo de Planning:", error);
-    
   }
 };
